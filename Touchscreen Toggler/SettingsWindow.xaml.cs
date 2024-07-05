bc524451319a6a4710a169385db6bcc3d936b1ce
Sverd Industries <eirik.sverd@gmail.com>
2024-07-05 15:04:08 -0500

    added system.management dependency

--- Touchscreen Toggler/SettingsWindow.xaml.cs
+++ Touchscreen Toggler/SettingsWindow.xaml.cs
@@ -22,6 +22,32 @@ namespace Touchscreen_Toggler
         public SettingsWindow()
         {
             InitializeComponent();
+            LoadDevices();
+        }
+
+        private void LoadDevices()
+        {
+            DeviceList.Items.Clear();
+            string query = "SELECT * FROM Win32_PnPEntity WHERE Name LIKE '%HID-compliant touch screen%'";
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(query);
+            foreach (ManagementObject device in searcher.Get())
+            {
+                DeviceList.Items.Add(device["Name"]);
+            }
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoadDevices();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Save the selected device (for future use, if necessary)
+            // This could involve saving to a configuration file or similar
+            MessageBox.Show("Device saved");
+            Close();
         }
     }
+
 }
